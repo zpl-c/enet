319d0e5b0db8504ef105a939ce068a59bded3e62
oylbin <oylbin@gmail.com>
2024-03-01 16:52:26 +0800

    update test to make length more clear

--- test/resize.c
+++ test/resize.c
@@ -2,10 +2,15 @@
 #include "enet.h"
 int main() {
     ENetPacket *packet = enet_packet_create("packet",
-                                            strlen("packet") + 1,
+                                            strlen("packet"),
                                             ENET_PACKET_FLAG_RELIABLE);
     printf("length: %d, data: %.*s\n", packet->dataLength, packet->dataLength, (char*)packet->data);
-    packet = enet_packet_resize(packet, strlen("packetfoo") + 1);
+    packet = enet_packet_resize(packet, strlen("packetfoo"));
     strcpy (& packet -> data [strlen ("packet")], "foo");
+    printf("length: %d, data: %.*s\n", packet->dataLength, packet->dataLength, (char*)packet->data);
+    packet = enet_packet_resize(packet, strlen("packet"));
+    printf("length: %d, data: %.*s\n", packet->dataLength, packet->dataLength, (char*)packet->data);
+    packet = enet_packet_resize(packet, strlen("packetfoobar"));
+    strcpy (& packet -> data [strlen ("packet")], "foobar");
     printf("length: %d, data: %.*s\n", packet->dataLength, packet->dataLength, (char*)packet->data);
 }
